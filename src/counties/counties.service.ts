c299ae7814193c54bd72da55d79c157681edadbd
sebastianhanberg <sebastian.hanberg@gmail.com>
2024-09-16 13:37:10 +0200

    kommuner

--- src/counties/counties.service.ts
+++ src/counties/counties.service.ts
@@ -1,31 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-
-// @Injectable()
-// export class CountiesService {
-//   // This is a mock. In a real application, you'd fetch this from a database.
-//   private counties = [
-//     {
-//       name: 'Stockholm',
-//       center: [59.3293, 18.0686],
-//       population: 935619,
-//       area: 188,
-//     },
-//     {
-//       name: 'Göteborg',
-//       center: [57.7089, 11.9746],
-//       population: 579281,
-//       area: 447,
-//     },
-//     // Add more counties here
-//   ];
-
-//   async searchCounty(query: string) {
-//     return this.counties.find((county) =>
-//       county.name.toLowerCase().includes(query.toLowerCase()),
-//     );
-//   }
-// }
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, Like } from 'typeorm';
